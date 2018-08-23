84193d50b8b414c2e41fbc99bfb839bea93556bc
RaviSoji <raviisojitraa@Gmail.com>
2018-08-23 10:03:44 -0400

    Comment out explanation code until test code is completed.

--- btplda/category_explainer.py
+++ btplda/category_explainer.py
@@ -1,14 +1,14 @@
-class Explainer:
-    def __init__(self, fitted_plda_classifier):
-        self.model = 
-
-
-    def explain_category(self, category, n_ts=5, ts_sz=1):
-        assert category in self.model.get_categories
-
-        teaching_sets = 
-
-        highlighter = FeatureHighlighter(
-        highlighted, filters = 
-
-        return teaching_sets, highlighted_teaching_sets
+# class Explainer:
+#     def __init__(self, fitted_plda_classifier):
+#         self.model = 
+# 
+# 
+#     def explain_category(self, category, n_ts=5, ts_sz=1):
+#         assert category in self.model.get_categories
+# 
+#         teaching_sets = 
+# 
+#         highlighter = FeatureHighlighter(
+#         highlighted, filters = 
+# 
+#         return teaching_sets, highlighted_teaching_sets
